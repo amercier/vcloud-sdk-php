34cfa39352eb02de7489ac52545cc64ecd337869
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-08-05 16:23:22 +0200

    Added abstract class VCloudTest

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,15 @@
 <?php
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
+
+abstract class VCloudTest extends PHPUnit_Framework_TestCase
+{
+    const CONFIG_FILE = '/config.json';
+
+    protected $config = null;
+
+    public function setUp()
+    {
+        $this->config = json_decode( file_get_contents(dirname(__FILE__) . self::CONFIG_FILE), true );
+    }
+}
