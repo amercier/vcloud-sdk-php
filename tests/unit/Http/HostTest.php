e33d1c5ab97596d07f48c4c6f74d170fcc4028df
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-08-06 17:42:03 +0200

    Fixed Host tests

--- tests/unit/Http/HostTest.php
+++ tests/unit/Http/HostTest.php
@@ -9,13 +9,23 @@ class HostTest extends VCloudTest
     public function testConstructWithFQDN()
     {
         new Host('http://server.example.org');
+        $this->assertTrue(true);
         new Host('https://server.example.org');
+        $this->assertTrue(true);
     }
 
     public function testConstructWithIP()
     {
         new Host('http://192.168.0.1');
+        $this->assertTrue(true);
         new Host('https://192.168.0.1');
+        $this->assertTrue(true);
+    }
+
+    public function testGetUrl()
+    {
+        $host = new Host('http://server.example.org');
+        $this->assertEquals($host->getUrl(), 'http://server.example.org');
     }
 
     /**
