02740fbaecebd82e09186c5f46b1ec80fd49518c
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-08-06 16:04:33 +0200

    Implemented missing Exceptions

---
+++ src/VMware/VCloud/Exception/InvalidKey.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VMware\VCloud\Exception;
+
+use Cli\Helpers\Exception as VCloudException;
+
+class InvalidKey extends VCloudException
+{
+}
---
+++ src/VMware/VCloud/Exception/MalformedUrl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VMware\VCloud\Exception;
+
+use Cli\Helpers\Exception as VCloudException;
+
+class MalformedUrl extends VCloudException
+{
+}
