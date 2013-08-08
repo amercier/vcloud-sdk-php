9271493ed09f77efc9d50e56340a4c23a5ec6970
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-08-08 14:20:14 +0200

    Improved Host

--- src/VMware/VCloud/Http/Host.php
+++ src/VMware/VCloud/Http/Host.php
@@ -13,13 +13,16 @@ class Host extends AbstractObject
     {
         $fragments = parse_url($url);
         if ($fragments === false
-            || !array_key_exists('scheme', $fragments)
-            || !array_key_exists('host', $fragments)
-            || array_key_exists('path', $fragments)
+            || array_key_exists('host', $fragments)
+            || array_key_exists('scheme', $fragments)
+            || array_key_exists('port', $fragments)
+            || !array_key_exists('path', $fragments)
             || array_key_exists('query', $fragments)
             || array_key_exists('fragment', $fragments)
+            || strpos($url, ' ') !== false
+            || strpos($url, '/') !== false
         ) {
-            throw new MalformedUrl($url);
+            throw new MalformedUrl($url . ' => ' . print_r($fragments, true));
         }
 
         $this->set('url', $url);
--- tests/unit/Http/HostTest.php
+++ tests/unit/Http/HostTest.php
@@ -8,24 +8,20 @@ class HostTest extends VCloudTest
 {
     public function testConstructWithFQDN()
     {
-        new Host('http://server.example.org');
-        $this->assertTrue(true);
-        new Host('https://server.example.org');
+        new Host('server.example.org');
         $this->assertTrue(true);
     }
 
     public function testConstructWithIP()
     {
-        new Host('http://192.168.0.1');
-        $this->assertTrue(true);
-        new Host('https://192.168.0.1');
+        new Host('192.168.0.1');
         $this->assertTrue(true);
     }
 
     public function testGetUrl()
     {
-        $host = new Host('http://server.example.org');
-        $this->assertEquals($host->getUrl(), 'http://server.example.org');
+        $host = new Host('server.example.org');
+        $this->assertEquals($host->getUrl(), 'server.example.org');
     }
 
     /**
@@ -34,5 +30,45 @@ class HostTest extends VCloudTest
     public function testMalformedUrl()
     {
         new Host('mer il et fou');
+    }
+
+    /**
+     * @expectedException \VMware\VCloud\Exception\MalformedUrl
+     */
+    public function testUrlWithScheme()
+    {
+        new Host('http://server.example.org');
+    }
+
+    /**
+     * @expectedException \VMware\VCloud\Exception\MalformedUrl
+     */
+    public function testUrlWithPort()
+    {
+        new Host('server.example.org:443');
+    }
+
+    /**
+     * @expectedException \VMware\VCloud\Exception\MalformedUrl
+     */
+    public function testUrlWithPath()
+    {
+        new Host('server.example.org/test');
+    }
+
+    /**
+     * @expectedException \VMware\VCloud\Exception\MalformedUrl
+     */
+    public function testUrlWithQuery()
+    {
+        new Host('server.example.org?ok=false');
+    }
+
+    /**
+     * @expectedException \VMware\VCloud\Exception\MalformedUrl
+     */
+    public function testUrlWithFragment()
+    {
+        new Host('server.example.org#nok');
     }
 }
