b89cb008570193f99cb4a6f46b1c739c16a8d360
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-08-21 10:52:50 +0200

    Fixed wrong assertEquals parameters order

--- tests/unit/Ip/AddressTest.php
+++ tests/unit/Ip/AddressTest.php
@@ -33,10 +33,10 @@ class AddressTest extends VCloudTest
         $address3 = new Address('255.255.255.255');
         $address4 = new Address('192.168.0.1');
 
-        $this->assertEquals('' . $address1, '0.0.0.0'        , "new Address('0.0.0.0').toString() === '0.0.0.0'");
-        $this->assertEquals('' . $address2, '255.255.255.0'  , "new Address('255.255.255.0').toString() === '255.255.255.0'");
-        $this->assertEquals('' . $address3, '255.255.255.255', "new Address('255.255.255.255').toString() === '255.255.255.255'");
-        $this->assertEquals('' . $address4, '192.168.0.1'    , "new Address('192.168.0.1').toString() === '192.168.0.1'");
+        $this->assertEquals('0.0.0.0'        , '' . $address1, "new Address('0.0.0.0').toString() === '0.0.0.0'");
+        $this->assertEquals('255.255.255.0'  , '' . $address2, "new Address('255.255.255.0').toString() === '255.255.255.0'");
+        $this->assertEquals('255.255.255.255', '' . $address3, "new Address('255.255.255.255').toString() === '255.255.255.255'");
+        $this->assertEquals('192.168.0.1'    , '' . $address4, "new Address('192.168.0.1').toString() === '192.168.0.1'");
     }
 
     public function testGetNext()
@@ -56,17 +56,17 @@ class AddressTest extends VCloudTest
         $address5a = new Address('127.255.255.255');
         $address5b = new Address('128.0.0.0');
 
-        $this->assertEquals($address1a->getNext(), $address1b, "new Address('0.0.0.0')->getNext() === '0.0.0.1'");
-        $this->assertEquals($address2a->getNext(), $address2b, "new Address('192.168.10.100')->getNext() === '192.168.10.101'");
-        $this->assertEquals($address3a->getNext(), $address3b, "new Address('192.168.0.255')->getNext() === '192.168.1.0'");
-        $this->assertEquals($address4a->getNext(), $address4b, "new Address('192.255.255.255')->getNext() === '193.0.0.0'");
-        $this->assertEquals($address5a->getNext(), $address5b, "new Address('127.255.255.255')->getNext() === '128.0.0.0'");
+        $this->assertEquals($address1b, $address1a->getNext(), "new Address('0.0.0.0')->getNext() === '0.0.0.1'");
+        $this->assertEquals($address2b, $address2a->getNext(), "new Address('192.168.10.100')->getNext() === '192.168.10.101'");
+        $this->assertEquals($address3b, $address3a->getNext(), "new Address('192.168.0.255')->getNext() === '192.168.1.0'");
+        $this->assertEquals($address4b, $address4a->getNext(), "new Address('192.255.255.255')->getNext() === '193.0.0.0'");
+        $this->assertEquals($address5b, $address5a->getNext(), "new Address('127.255.255.255')->getNext() === '128.0.0.0'");
 
-        $this->assertEquals('' . $address1a->getNext(), '' . $address1b, "new Address('0.0.0.0')->getNext() === '0.0.0.1'");
-        $this->assertEquals('' . $address2a->getNext(), '' . $address2b, "new Address('192.168.10.100')->getNext() === '192.168.10.101'");
-        $this->assertEquals('' . $address3a->getNext(), '' . $address3b, "new Address('192.168.0.255')->getNext() === '192.168.1.0'");
-        $this->assertEquals('' . $address4a->getNext(), '' . $address4b, "new Address('192.255.255.255')->getNext() === '193.0.0.0'");
-        $this->assertEquals('' . $address5a->getNext(), '' . $address5b, "new Address('127.255.255.255')->getNext() === '128.0.0.0'");
+        $this->assertEquals('' . $address1b, '' . $address1a->getNext(), "new Address('0.0.0.0')->getNext() === '0.0.0.1'");
+        $this->assertEquals('' . $address2b, '' . $address2a->getNext(), "new Address('192.168.10.100')->getNext() === '192.168.10.101'");
+        $this->assertEquals('' . $address3b, '' . $address3a->getNext(), "new Address('192.168.0.255')->getNext() === '192.168.1.0'");
+        $this->assertEquals('' . $address4b, '' . $address4a->getNext(), "new Address('192.255.255.255')->getNext() === '193.0.0.0'");
+        $this->assertEquals('' . $address5b, '' . $address5a->getNext(), "new Address('127.255.255.255')->getNext() === '128.0.0.0'");
     }
 
     /**
@@ -95,17 +95,17 @@ class AddressTest extends VCloudTest
         $address5a = new Address('128.0.0.0');
         $address5b = new Address('127.255.255.255');
 
-        $this->assertEquals($address1a->getPrevious(), $address1b, "new Address('255.255.255.255')->getPrevious() === '255.255.255.254'");
-        $this->assertEquals($address2a->getPrevious(), $address2b, "new Address('192.168.10.100')->getPrevious() === '192.168.10.99'");
-        $this->assertEquals($address3a->getPrevious(), $address3b, "new Address('192.168.1.0')->getPrevious() === '192.168.0.255'");
-        $this->assertEquals($address4a->getPrevious(), $address4b, "new Address('193.0.0.0')->getPrevious() === '192.255.255.255'");
-        $this->assertEquals($address5a->getPrevious(), $address5b, "new Address('128.0.0.0')->getPrevious() === '127.255.255.255'");
+        $this->assertEquals($address1b, $address1a->getPrevious(), "new Address('255.255.255.255')->getPrevious() === '255.255.255.254'");
+        $this->assertEquals($address2b, $address2a->getPrevious(), "new Address('192.168.10.100')->getPrevious() === '192.168.10.99'");
+        $this->assertEquals($address3b, $address3a->getPrevious(), "new Address('192.168.1.0')->getPrevious() === '192.168.0.255'");
+        $this->assertEquals($address4b, $address4a->getPrevious(), "new Address('193.0.0.0')->getPrevious() === '192.255.255.255'");
+        $this->assertEquals($address5b, $address5a->getPrevious(), "new Address('128.0.0.0')->getPrevious() === '127.255.255.255'");
 
-        $this->assertEquals('' . $address1a->getPrevious(), '' . $address1b, "new Address('255.255.255.255')->getPrevious() === '255.255.255.254'");
-        $this->assertEquals('' . $address2a->getPrevious(), '' . $address2b, "new Address('192.168.10.100')->getPrevious() === '192.168.10.99'");
-        $this->assertEquals('' . $address3a->getPrevious(), '' . $address3b, "new Address('192.168.1.0')->getPrevious() === '192.168.0.255'");
-        $this->assertEquals('' . $address4a->getPrevious(), '' . $address4b, "new Address('193.0.0.0')->getPrevious() === '192.255.255.255'");
-        $this->assertEquals('' . $address5a->getPrevious(), '' . $address5b, "new Address('128.0.0.0')->getPrevious() === '127.255.255.255'");
+        $this->assertEquals('' . $address1b, '' . $address1a->getPrevious(), "new Address('255.255.255.255')->getPrevious() === '255.255.255.254'");
+        $this->assertEquals('' . $address2b, '' . $address2a->getPrevious(), "new Address('192.168.10.100')->getPrevious() === '192.168.10.99'");
+        $this->assertEquals('' . $address3b, '' . $address3a->getPrevious(), "new Address('192.168.1.0')->getPrevious() === '192.168.0.255'");
+        $this->assertEquals('' . $address4b, '' . $address4a->getPrevious(), "new Address('193.0.0.0')->getPrevious() === '192.255.255.255'");
+        $this->assertEquals('' . $address5b, '' . $address5a->getPrevious(), "new Address('128.0.0.0')->getPrevious() === '127.255.255.255'");
     }
 
     /**
