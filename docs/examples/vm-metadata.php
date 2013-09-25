b8d4c89f2f684d1c9d61918b3fef25d2a5aa36bc
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-09-25 16:44:27 +0200

    Added vm-metadata script

---
+++ docs/examples/vm-metadata.php
@@ -0,0 +1,58 @@
+#!/usr/bin/env php
+<?php
+
+require_once dirname(__FILE__) . '/config/bootstrap.php';
+
+$service = new VMware\VCloud\Service($config['host']);
+
+Cli\Helpers\Job::run('Authenticating against ' . $config['host'], function() {
+    global $service, $config;
+    $service->login($config['cloudadmin']);
+});
+
+if ($service->isLoggedIn()) {
+
+    echo "\n";
+
+    // die(print_r($service->queryReferences('adminVM'), true));
+
+    $vms =  $service->getAllVirtualMachines();
+    // die(get_class($vms[0]));
+    // die(count($vms) . "\n");
+    // die(print_r($vms[0]->getMetadataSet(), true));
+
+    foreach ($vms as $vm) {
+        if (count($vm->getMetadata()->getEntries()) === 0) {
+        }
+        else {
+            echo " ▸ " . $vm . "\n";
+            $entries = $vm->getMetadata()->getEntries();
+
+            $entriesNamesMaxLength = max(array_map(function($entry) {
+                return strlen('' . $entry->getName());
+            }, $entries));
+            $entriesValuesMaxLength = max(array_map(function($entry) {
+                return strlen('' . $entry->getValue());
+            }, $entries));
+            $entriesTypesMaxLength = max(array_map(function($entry) {
+                return strlen('' . $entry->getType());
+            }, $entries));
+            $entriesDomainsMaxLength = max(array_map(function($entry) {
+                return strlen('' . $entry->getDomain());
+            }, $entries));
+            $entriesVisibilitiesMaxLength = max(array_map(function($entry) {
+                return strlen('' . $entry->getVisibility());
+            }, $entries));
+
+            foreach($entries as $entry) {
+                echo "    ▸ " ;
+                echo str_pad($entry->getName(), $entriesNamesMaxLength) . "   ";
+                echo str_pad($entry->getValue(), $entriesValuesMaxLength) . "   ";
+                echo str_pad($entry->getType(), $entriesTypesMaxLength) . "   ";
+                echo str_pad($entry->getDomain(), $entriesDomainsMaxLength) . "   ";
+                echo str_pad($entry->getVisibility(), $entriesVisibilitiesMaxLength) . "   ";
+                echo "\n";
+            }
+        }
+    }
+}
