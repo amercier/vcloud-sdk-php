da36a18ec93a567c34cac47b306f990354707416
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-10-03 17:29:33 +0200

    Started Service rewrite

--- src/VMware/VCloud/Service.php
+++ src/VMware/VCloud/Service.php
@@ -8,14 +8,16 @@ class Service extends Object
 {
     const QUERY_PAGE_SIZE = 128;
 
-    protected $implementation;
+    protected $sdkObject;
     protected $host;
     protected $httpConfiguration;
     protected $credentials;
-    protected $loggedIn = false;
-    protected $organizations = array();
+    protected $loggedIn;
+    protected $organizations;
     protected $externalNetworks;
-    protected $virtualMachines;
+
+    protected $dataObjectsRegistry;
+    protected $sdkObjectsRegistry;
 
     public function __construct($host, Http\Configuration $httpConfiguration = null)
     {
@@ -32,6 +34,71 @@ class Service extends Object
             ? Http\Configuration::getDefaultConfiguration()
             : $httpConfiguration
         );
+        $this->init();
+    }
+
+    protected function init()
+    {
+        $this->$loggedIn = false;
+        $this->$organizations = array();
+        $this->dataObjectsRegistry = new Map();
+        $this->sdkObjectsRegistry = new Map();
+    }
+
+    protected function getSdkObject()
+    {
+        return $this->get('sdkObject', 'createSdkObject');
+    }
+
+    protected function createSdkObject()
+    {
+        return \VMware_VCloud_SDK_Service::getService();
+    }
+
+    public function hasDataObject($href) {
+        return array_key_exists($href, $this->dataObjectsRegistry);
+    }
+
+    public function getDataObject($href, $exceptionIfNotFound = true)
+    {
+        return $this->getByKey(
+            'dataObjectsRegistry',
+            $href,
+            $exceptionIfNotFound,
+            'Data object with href $value does not exist in registry'
+        );
+    }
+
+    public function addDataObject(\VMware_VCloud_API_IdentifiableResourceType $dataObject)
+    {
+        $href = $dataObject->get_href();
+        if (!$this->hasDataObject($href)) {
+            $this->dataObjectsRegistry->set($href, $dataObject);
+        }
+        return $this;
+    }
+
+    public function hasSdkObject($href) {
+        return array_key_exists($href, $this->sdkObjectsRegistry);
+    }
+
+    public function getSdkObject($href, $exceptionIfNotFound = true)
+    {
+        return $this->getByKey(
+            'sdkObjectsRegistry',
+            $href,
+            $exceptionIfNotFound,
+            'SDK object with href $value does not exist in registry'
+        );
+    }
+
+    public function addSdkObject(\VMware_VCloud_API_IdentifiableResourceType $sdkObject)
+    {
+        $href = $sdkObject->get_href();
+        if (!$this->hasDataObject($href)) {
+            $this->sdkObjectsRegistry->set($href, $sdkObject);
+        }
+        return $this;
     }
 
     public function getHost()
@@ -39,14 +106,14 @@ class Service extends Object
         return $this->get('host');
     }
 
-    public function getImplementation()
+    public function getCredentials()
     {
-        return $this->get('implementation', 'createImplementation');
+        return $this->get('credentials');
     }
 
-    protected function createImplementation()
+    public function getOrganizations()
     {
-        return \VMware_VCloud_SDK_Service::getService();
+        return $this->get('organizations');
     }
 
     public function isLoggedIn()
@@ -98,250 +165,3 @@ class Service extends Object
         return $this->getImplementation()->getToken();
     }
 
-    public function createImplementationFromReference(\VMware_VCloud_API_ReferenceType $reference)
-    {
-        return $this->getImplementation()->createSdkObj($reference);
-    }
-
-    public function createImplementationFromEntity(\VMware_VCloud_API_EntityType $entity)
-    {
-        return $this->getImplementation()->createSdkObj($entity);
-    }
-
-    /*
-    public function createModelFromResource(\VMware_VCloud_API_ResourceEntityType $resource)
-    {
-        return $this->getImplementation()->createSdkObj($resource);
-    }
-    */
-
-    public function getCredentials()
-    {
-        return $this->get('credentials');
-    }
-
-    public function getOrganizations()
-    {
-        return $this->get('organizations');
-    }
-
-    public function getOrganizationById($id, $exceptionIfNotFound = true)
-    {
-        return $this->getBy(
-            'organizations',
-            'id',
-            $id,
-            'Organization',
-            'vCloud Director ' . $this->getHost(),
-            $exceptionIfNotFound
-        );
-    }
-
-    public function getOrganizationByName($name, $exceptionIfNotFound = true)
-    {
-        return $this->getBy(
-            'organizations',
-            'name',
-            strtolower($name),
-            'Organization',
-            'vCloud Director ' . $this->getHost(),
-            $exceptionIfNotFound
-        );
-    }
-
-    public function getCurrentOrganization()
-    {
-        return $this->getOrganizationByName($this->getCredentials()->getOrganization());
-    }
-
-    public function getExternalNetworks()
-    {
-        return $this->get('externalNetworks', 'retrieveExternalNetworks');
-    }
-
-    protected function retrieveExternalNetworks()
-    {
-        $externalNetworks = array();
-        foreach ($this->getImplementation()->createSDKAdminObj()->getExternalNetworkRefs() as $externalNetworkRef) {
-            array_push($externalNetworks, new ExternalNetwork($this, null, $externalNetworkRef));
-        }
-        return $externalNetworks;
-    }
-
-    public function getExternalNetworkById($id, $exceptionIfNotFound = true)
-    {
-        return $this->getBy(
-            'externalNetworks',
-            'id',
-            $id,
-            'External Network',
-            'vCloud Director ' . $this->getHost(),
-            $exceptionIfNotFound
-        );
-    }
-
-    public function getExternalNetworkByName($name, $exceptionIfNotFound = true)
-    {
-        return $this->getBy(
-            'externalNetworks',
-            'name',
-            $name,
-            'External Network',
-            'vCloud Director ' . $this->getHost(),
-            $exceptionIfNotFound
-        );
-    }
-
-    protected function queryRecords($type)
-    {
-        $records = array();
-
-        for ($page = 1, $continue = true; $continue; $page++) {
-
-            $params = new \VMware_VCloud_SDK_Query_Params();
-            $params->setPageSize(self::QUERY_PAGE_SIZE);
-            $params->setPage($page);
-
-            $queryService = $this->getImplementation()->getQueryService();
-            $result = $queryService->queryRecords($type, $params);
-
-            $records = array_merge($records, $result->getRecord());
-
-            $continue = in_array(
-                'lastPage',
-                array_map(
-                    function ($link) {
-                        return $link->get_rel();
-                    },
-                    $result->getLink()
-                )
-            );
-        }
-
-        return $records;
-    }
-
-    protected function queryReferences($type)
-    {
-        $references = array();
-
-        for ($page = 1, $continue = true; $continue; $page++) {
-
-            $params = new \VMware_VCloud_SDK_Query_Params();
-            $params->setPageSize(self::QUERY_PAGE_SIZE);
-            $params->setPage($page);
-
-            $queryService = $this->getImplementation()->getQueryService();
-            $result = $queryService->queryReferences($type, $params);
-
-            $references = array_merge($references, $result->getReference());
-
-            $continue = in_array(
-                'lastPage',
-                array_map(
-                    function ($link) {
-                        return $link->get_rel();
-                    },
-                    $result->getLink()
-                )
-            );
-        }
-
-        return $references;
-    }
-
-    public function getAllVirtualMachines()
-    {
-        $result = $this->get('virtualMachines', 'retrieveAllVirtualMachines');
-        return $result;
-    }
-
-    protected function retrieveAllVirtualMachines()
-    {
-        $virtualMachines = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ADMIN_VM) as $record) {
-            if (!$record->get_isVAppTemplate()) {
-                $virtualMachine = VirtualMachine::factory($record, $this);
-                $virtualMachines[$virtualMachine->getId()] = $virtualMachine;
-            }
-        }
-        return $virtualMachines;
-    }
-
-    public function getVirtualMachineById($id, $exceptionIfNotFound = true)
-    {
-        $virtualMachines = $this->getAllVirtualMachines();
-        return array_key_exists($id, $virtualMachines) ? $virtualMachines[$id] : null;
-        // foreach ($this->getAllVirtualMachines() as $virtualMachine) {
-        //     if ($virtualMachine->getId() === $id) {
-        //         return $virtualMachine;
-        //     }
-        // }
-        // return null;
-    }
-
-    public function getAllOrganizations()
-    {
-        // $Organizations = array();
-        // foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ORGANIZATION) as $record) {
-        //     array_push($Organizations, Organization::factory($record, $this));
-        // }
-        // return $Organizations;
-        return $this->getOrganizations();
-    }
-
-    public function getAllVirtualDataCenters()
-    {
-        $virtualDataCenters = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ADMIN_ORG_VDC) as $record) {
-            array_push($virtualDataCenters, VirtualDataCenter::factory($record, $this));
-        }
-        return $virtualDataCenters;
-    }
-
-    public function getAllExternalNetworks()
-    {
-        $externalNetworks = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::EXTERNAL_NETWORK) as $record) {
-            array_push($externalNetworks, ExternalNetwork::factory($record, $this));
-        }
-        return $externalNetworks;
-    }
-
-    public function getAllVApps()
-    {
-        $vApps = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ADMIN_VAPP) as $record) {
-            array_push($vApps, VApp::factory($record, $this));
-        }
-        return $vApps;
-    }
-
-    public function getAllVAppTemplates()
-    {
-        $vAppTemplates = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ADMIN_VAPP_TEMPLATE) as $record) {
-            array_push($vAppTemplates, VAppTemplate::factory($record, $this));
-        }
-        return $vAppTemplates;
-    }
-
-    public function getAllOrganizationNetworks()
-    {
-        $organizationNetworks = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ADMIN_ORG_NETWORK) as $record) {
-            array_push($organizationNetworks, OrganizationNetwork::factory($record, $this));
-        }
-        return $organizationNetworks;
-    }
-
-
-    public function getAllCatalogs()
-    {
-        $catalogs = array();
-        foreach ($this->queryRecords(\VMware_VCloud_SDK_Query_Types::ADMIN_CATALOG) as $record) {
-            array_push($catalogs, Catalog::factory($record, $this));
-        }
-        return $catalogs;
-    }
-}
